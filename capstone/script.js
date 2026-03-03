76a76bdc3879c998b4e90a44487471589fdf716a
Raya <125909608+coldravioli@users.noreply.github.com>
2026-03-03 12:58:49 -0500

    going

---
+++ capstone/script.js
@@ -0,0 +1,39 @@
+const phrase1 = "Our website is currently in the works."; 
+const phrase2 = "...";
+const phrase3 = "Please come back later.";
+
+const texts = [phrase1, phrase2, phrase3];
+
+const terminalElement = document.querySelector(".terminal-text");
+const clearButton = document.getElementById("clear-button");
+
+let currentTextIndex = 0;
+let i = 0;
+
+function typeText() {
+  if (i < texts[currentTextIndex].length) {
+    terminalElement.innerHTML += texts[currentTextIndex].charAt(i);
+    i++;
+    setTimeout(typeText, 60);
+  } else {
+    terminalElement.classList.add("blink"); 
+    setTimeout(() => {
+      currentTextIndex++;
+      if (currentTextIndex < texts.length) {
+        terminalElement.innerHTML = ''; 
+        terminalElement.classList.remove("blink"); 
+        i = 0; 
+        setTimeout(typeText, 700); 
+      } else {
+        terminalElement.classList.remove("blink"); 
+        // clearButton.style.display = 'block'; 
+      }
+    }, 2000); 
+  }
+}
+
+clearButton.addEventListener('click', () => {
+  window.location.href = 'room.html'; 
+});
+
+window.onload = typeText;
